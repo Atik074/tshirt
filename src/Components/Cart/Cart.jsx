018d9f464f2d2efe56fa7e26a5c229a3709e6eab
Atik074 <atiksmats@gmail.com>
2023-04-10 03:04:39 +0600

    added toast

--- src/Components/Cart/Cart.jsx
+++ src/Components/Cart/Cart.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import './Cart.css'
 
-const Cart = () => {
+const Cart = ({cart,handleRemoveFromCart}) => {
+    console.log(cart)
     return (
         <div>
-            <h2>output from Cart</h2>
+            <h2>output from Cart {cart.length}</h2>
+            {cart.map(tshirt =><p 
+            key={tshirt._id}>
+                {tshirt.name}
+                <button onClick={()=>{handleRemoveFromCart(tshirt._id)}}>Delete</button>
+            </p>)}
         </div>
     );
 };
--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -3,12 +3,27 @@ import './Home.css'
 import { useLoaderData } from 'react-router-dom';
 import Tshirt from '../Tshirt/Tshirt';
 import Cart from '../Cart/Cart';
+import toast from 'react-hot-toast';
+
 
 const Home = () => {
     const [cart,setCart] =useState([])
 
     const handleAddtoCart = (tshirt)=>{
-        console.log(tshirt)
+      const exists =  cart.find(ts=> ts._id === tshirt._id )
+      if(exists){
+        toast('already added')
+      }
+      else{
+        const newCart =[...cart , tshirt]
+        setCart(newCart)
+      }
+       
+    }
+    const handleRemoveFromCart =(id)=>{
+        const remaining = cart.filter(ts => ts._id !== id)
+        setCart(remaining)
+       
     }
     const tShirts =useLoaderData()
 
@@ -25,7 +40,11 @@ const Home = () => {
            </div>
 
            <div className='cart-container'>
-            <Cart></Cart>
+            <Cart
+            cart={cart}
+            handleRemoveFromCart={handleRemoveFromCart}
+            
+            ></Cart>
            </div>
 
            
